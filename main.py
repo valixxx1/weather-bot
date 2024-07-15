19ece7fd633e7ff43dd532bb94c5bd25459f58a5
vgershun <vgershun@MacBook-Pro-vgershun.local>
2024-07-15 20:16:42 +0300

    new-bot

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-with open('weather.key', 'r') as f:
-    print(f.read())
+import telebot
+
+with open('telebot.key', 'r') as f:
+    token = f.read()
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    text = "Welcome! This bot can tell you about weather."
+    bot.send_message(message.chat.id, text)
+
+bot.polling()
